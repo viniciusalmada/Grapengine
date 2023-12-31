17544c868d290e1407d3a72da4ec76a8b24af8c5
Vinicius Almada <viniciusalmada10@gmail.com>
2023-12-31 19:05:58 -0300

    create a render batch test in main function

--- main.cpp
+++ main.cpp
@@ -5,25 +5,43 @@
 #include <ge_circle_shape.hpp>
 #include <ge_rect_shape.hpp>
 #include <iostream>
+#include <chrono>
 
 int main()
 {
-  Application application{ "Graphic Engine", 512, 512 };
+  u32 dim = 800;
+  Application application{ "Graphic Engine", dim, dim };
 
-  auto d = MakeRef<RectShape>(-0.5f, -0.5f, 0.4f, 0.2f);
-  auto c = MakeRef<CircleShape>(+0.5f, +0.5f, 0.4f);
-
-  float vel = 0.0025f;
+  constexpr u32 side_circles = 10;
+  float x = dim / (float)side_circles * 0.1f;
+  float y = dim / (float)side_circles * 0.1f;
+  float radius = (dim / (float)side_circles * 0.8f) / 2.0f;
+  std::vector<Ref<CircleShape>> rects;
+  rects.reserve(side_circles * side_circles);
+  auto start = std::chrono::high_resolution_clock::now();
+  for (u32 i = 0; i < side_circles; ++i)
+  {
+    for (u32 j = 0; j < side_circles; ++j)
+    {
+      auto c = MakeRef<CircleShape>(x + radius, y + radius, radius);
+      rects.push_back(c);
+      x += dim / (float)side_circles;
+    }
+    x = dim / (float)side_circles * 0.1f;
+    y += dim / (float)side_circles;
+    std::cout << i / (float)side_circles * 100.0f << "% completed" << std::endl;
+  }
+  std::cout << "Finish objects allocations" << std::endl;
+  auto end = std::chrono::high_resolution_clock::now();
+  auto ms_int = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+  std::cout << "Costed " << ms_int << std::endl;
 
   application.Run(
     [&](Window& window)
     {
-      d->SetPosition(d->GetPosition().x + vel, d->GetPosition().y + vel);
-      d->SetPosition(d->GetPosition().x + vel, d->GetPosition().y + vel);
-
       window.Clear();
-      window.Draw(d);
-      window.Draw(c);
+      for (const auto& item : rects)
+        window.Draw(item);
     });
   return 0;
 }
