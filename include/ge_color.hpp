aece573a83401d5d7aa2223d8d4efec0c6dd2013
Vinicius Almada <viniciusalmada10@gmail.com>
2023-12-31 19:04:47 -0300

    create Color struct

---
+++ include/ge_color.hpp
@@ -0,0 +1,14 @@
+#ifndef GRAPHICENGINE3D_GE_COLOR_HPP
+#define GRAPHICENGINE3D_GE_COLOR_HPP
+
+#include "ge_using.hpp"
+
+struct Color
+{
+  u8 R = 0x00;
+  u8 G = 0x00;
+  u8 B = 0x00;
+  u8 A = 0xFF;
+};
+
+#endif // GRAPHICENGINE3D_GE_COLOR_HPP
