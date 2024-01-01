d034ec03f76ebf2de4d0cff9d2b1b02305c7b374
Vinicius Almada <viniciusalmada10@gmail.com>
2024-01-01 15:19:15 -0300

    Revert "wip: create a memory monitor"
    This reverts commit 3ba18a3e6b664d9cfaeb3b8b9e4ded783162b201.

--- include/ge_memory.hpp
+++ include/ge_memory.hpp
@@ -1,27 +1,18 @@
 #ifndef GRAPHICENGINE3D_GE_MEMORY_HPP
 #define GRAPHICENGINE3D_GE_MEMORY_HPP
 
-#include "ge_memory_monitor.hpp"
 #include "ge_using.hpp"
-
-void* operator new(u64 size);
-void operator delete (void* ptr)noexcept;
-
 
 template <class T, class... Args>
 Ref<T> MakeRef(Args... args)
 {
-  auto ptr = std::make_shared<T>(std::forward<Args>(args)...);
-  Mem::RegisterMemoryAllocation(ptr.get(), typeid(T).name(), sizeof(T));
-  return ptr;
+  return std::make_shared<T>(std::forward<Args>(args)...);
 }
 
 template <class T, class... Args>
 Scope<T> MakeScope(Args... args)
 {
-  auto ptr = std::make_unique<T>(std::forward<Args>(args)...);
-  Mem::RegisterMemoryAllocation(ptr.get(), typeid(T).name(), sizeof(T));
-  return ptr;
+  return std::make_unique<T>(std::forward<Args>(args)...);
 }
 
 #endif // GRAPHICENGINE3D_GE_MEMORY_HPP
--- include/ge_pch.hpp
+++ include/ge_pch.hpp
@@ -1,7 +1,6 @@
 #ifndef GRAPHIC_ENGINE3D_GE_PCH_HPP
 #define GRAPHIC_ENGINE3D_GE_PCH_HPP
 
-#include <algorithm>
 #include <any>
 #include <array>
 #include <bit>
@@ -12,10 +11,8 @@
 #include <functional>
 #include <iomanip>
 #include <iostream>
-#include <map>
 #include <memory>
 #include <numbers>
-#include <numeric>
 #include <random>
 #include <ranges>
 #include <sstream>
