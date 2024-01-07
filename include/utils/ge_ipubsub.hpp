23c872c6c8c009dc7603ea0dbb36113436d21468
Vinicius Almada <viniciusalmada10@gmail.com>
2024-01-07 18:23:37 -0300

    create PublishSubscriber interface

---
+++ include/utils/ge_ipubsub.hpp
@@ -0,0 +1,30 @@
+#ifndef GRAPHICENGINE3D_GE_IPUBSUB_HPP
+#define GRAPHICENGINE3D_GE_IPUBSUB_HPP
+
+template <typename Msg>
+class ISubscriber;
+
+template <class Msg>
+class IPublisher
+{
+public:
+  void Subscribe(Ref<ISubscriber<Msg>> sub) { m_subs.push_back(sub); };
+  void Unsubscribe(Ref<ISubscriber<Msg>> sub) { m_subs.remove(sub); };
+  virtual void Publish(Msg m) const
+  {
+    for (const auto& item : m_subs)
+      item->OnUpdate(m);
+  }
+
+private:
+  std::list<Ref<ISubscriber<Msg>>> m_subs;
+};
+
+template <class Msg>
+class ISubscriber
+{
+public:
+  virtual void OnUpdate(Msg data) = 0;
+};
+
+#endif // GRAPHICENGINE3D_GE_IPUBSUB_HPP
