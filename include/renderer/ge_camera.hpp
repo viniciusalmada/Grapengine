15ec6985bd1da74c5edbe1579f18c7d2ebf9b39d
Vinicius Almada <viniciusalmada10@gmail.com>
2024-01-14 18:51:19 -0300

    add controllers and limiters to Camera class

--- include/renderer/ge_camera.hpp
+++ include/renderer/ge_camera.hpp
@@ -6,12 +6,18 @@
 class Camera
 {
 public:
-  GE3D Camera(const Vec3& eye, float pitch, float yaw);
-  GE3D Camera(const Vec3& eye, const Vec3& dir);
+  GE3D Camera(float aspectRatio, const Vec3& eye, float pitch, float yaw);
+  GE3D Camera(float aspectRatio, const Vec3& eye, const Vec3& dir);
   GE3D ~Camera();
   Camera& operator=(const Camera&);
 
-  GE3D Mat4 GetViewProjection(u32 width, u32 height) const;
+  [[nodiscard]] bool IsMoving() const;
+  void StartMovement(Vec2 referencePoint) const;
+  void StopMovement() const;
+  void MeasureMovement(Vec2 currentPoint) const;
+
+  GE3D Mat4 GetViewProjection() const;
+  void SetAspectRatio(float ratio) const;
 
 private:
   POINTER_TO_IMPLEMENTATION_IDIOM
--- src/ge_camera.cpp
+++ src/ge_camera.cpp
@@ -5,47 +5,101 @@
 namespace
 {
   constexpr Vec3 UP_DIR = { 0.0f, 1.0f, 0.0f };
+  constexpr float FOV = 45;
+  constexpr float NEAR_FRUSTUM = 0.1;
+  constexpr float FAR_FRUSTUM = 100;
+
+  float ClampPitch(const float pitch)
+  {
+    return std::clamp(pitch, 91.0f, 269.0f);
+  }
+
+  Vec3 CalculateDirection(float pitch, const float yaw)
+  {
+    std::cout << std::format("{}", pitch) << std::endl;
+    // float yaw = 0;
+    const auto pitch_rad = Transform::Deg2Rad(pitch);
+    const auto yaw_rad = Transform::Deg2Rad(yaw);
+
+    // Pitch – plane YZ – 0 degree (camera back)
+    const float coord_z_pitch = std::cosf(pitch_rad);
+    const float coord_y_dir = std::sinf(pitch_rad);
+
+    const float& hypotenuse = coord_z_pitch;
+
+    // cosine of yaw = Z / Hypotenuse
+    const float coord_z_dir = std::cosf(yaw_rad) * hypotenuse;
+
+    // sine of yaw = X / Hypotenuse
+    const float coord_x_dir = std::sinf(yaw_rad) * hypotenuse;
+
+    return Vec3{ coord_x_dir, coord_y_dir, coord_z_dir };
+  }
 }
 
 struct Camera::Impl
 {
-  Vec3 eye;
-  Vec3 front;
+  float aspect_ratio = 1;
+  float sensibility = 0.1f;
+  Vec3 eye{};
+  Vec3 front{};
+  float pitch_angle = 0;
+  float yaw_angle = 0;
+  bool in_movement = false;
+  Vec2 movement_reference_pt{};
+
+  void CalculateEyeFront()
+  {
+    const auto dir = CalculateDirection(pitch_angle, yaw_angle);
+    front = eye + dir;
+  }
+
+  void UpdatePitchYaw(float diffPitch, float diffYaw)
+  {
+    if (in_movement)
+    {
+      const auto pitch = pitch_angle + diffPitch * sensibility;
+      const auto yaw = yaw_angle + diffYaw * sensibility;
+      front = eye + CalculateDirection(ClampPitch(pitch), yaw);
+      return;
+    }
+    pitch_angle += diffPitch * sensibility;
+    pitch_angle = ClampPitch(pitch_angle);
+    yaw_angle += diffYaw * sensibility;
+    CalculateEyeFront();
+  }
 };
 
-Camera::Camera(const Vec3& eye, const Vec3& dir) : m_pimpl(MakeScope<Impl>())
+Camera::Camera(const float aspectRatio, const Vec3& eye, const Vec3& dir) :
+    m_pimpl(MakeScope<Impl>())
 {
+  m_pimpl->aspect_ratio = aspectRatio;
   m_pimpl->eye = eye;
   m_pimpl->front = dir;
 }
 
-Camera::Camera(const Vec3& eye, float pitch, float yaw) : m_pimpl(MakeScope<Impl>())
+Camera::Camera(const float aspectRatio, const Vec3& eye, const float pitch, const float yaw) :
+    m_pimpl(MakeScope<Impl>())
 {
-  // Pitch – plane YZ – 0 degree (camera back)
-  const float coord_z_pitch = std::cosf(Transform::Deg2Rad(pitch));
-  const float coord_y_dir = std::sinf(Transform::Deg2Rad(pitch));
-
-  const float hypotenuse = coord_z_pitch;
-
-  // cosine of yaw = Z / Hypotenuse
-  const float coord_z_dir = std::cosf(Transform::Deg2Rad(yaw)) * hypotenuse;
-
-  // sine of yaw = X / Hypotenuse
-  const float coord_x_dir = std::sinf(Transform::Deg2Rad(yaw)) * hypotenuse;
-
-  auto dir = Vec3{ coord_x_dir, coord_y_dir, coord_z_dir };
-  this->m_pimpl->eye = eye;
-  this->m_pimpl->front = eye + dir;
+  m_pimpl->aspect_ratio = aspectRatio;
+  m_pimpl->eye = eye;
+  m_pimpl->pitch_angle = pitch;
+  m_pimpl->yaw_angle = yaw;
+  m_pimpl->CalculateEyeFront();
 }
 
 Camera::~Camera() = default;
 
-Mat4 Camera::GetViewProjection(const u32 width, const u32 height) const
+Mat4 Camera::GetViewProjection() const
 {
   const auto view = Transform::LookAt(m_pimpl->eye, m_pimpl->front, UP_DIR);
-  const auto projection =
-    Transform::Perspective(45, width / static_cast<float>(height), 0.1f, 100.0f);
-  return projection * view;
+  const auto proj = Transform::Perspective(FOV, m_pimpl->aspect_ratio, NEAR_FRUSTUM, FAR_FRUSTUM);
+  return proj * view;
+}
+
+void Camera::SetAspectRatio(const float ratio) const
+{
+  m_pimpl->aspect_ratio = ratio;
 }
 
 Camera& Camera::operator=(const Camera& other)
@@ -54,4 +108,26 @@ Camera& Camera::operator=(const Camera& other)
   this->m_pimpl->front = other.m_pimpl->front;
   this->m_pimpl->eye = other.m_pimpl->eye;
   return *this;
+}
+
+void Camera::StartMovement(Vec2 referencePoint) const
+{
+  m_pimpl->in_movement = true;
+  m_pimpl->movement_reference_pt = referencePoint;
+}
+
+bool Camera::IsMoving() const
+{
+  return m_pimpl->in_movement;
+}
+
+void Camera::StopMovement() const
+{
+  m_pimpl->in_movement = false;
+}
+
+void Camera::MeasureMovement(const Vec2 currentPoint) const
+{
+  const auto [x, y] = m_pimpl->movement_reference_pt - currentPoint;
+  m_pimpl->UpdatePitchYaw(y, -x);
 }
