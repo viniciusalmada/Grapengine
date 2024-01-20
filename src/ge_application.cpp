1ed5f448bd31d3d4d517414e32321eef37f90efa
Vinicius Almada <viniciusalmada10@gmail.com>
2024-01-19 22:56:40 -0300

    set up test of Camera in application

--- src/ge_application.cpp
+++ src/ge_application.cpp
@@ -14,20 +14,13 @@ class CameraChangePub : public IPublisher<Mat4>
 {
 };
 
-static float pitch = 180;
-static float yaw = 0;
-static Vec3 cam_pos{ 0, 0, 5 };
-
 struct Application::Impl
 {
   Scope<Window> window;
   bool running = true;
   bool minimized = false;
-  Camera camera{ cam_pos, pitch, yaw, 0 };
-  Ref<Texture2D> tex;
-  ResizeChangePub resize_change_pub{};
+  Scope<Camera> camera;
   CameraChangePub camera_change_pub{};
-  bool is_mouse_pressed = false;
   Vec2 mouse_press_pos{ 0, 0 };
 
   static Application* instance;
@@ -50,7 +43,8 @@ struct Application::Impl
                     {
                       const auto& [_, w, h] = *std::get_if<WindowResizeData>(&ev);
                       Renderer::SetViewport(0, 0, w, h);
-                      resize_change_pub.Publish({ (float)w, (float)h });
+                      camera->SetAspectRatio(static_cast<float>(w) / static_cast<float>(h));
+                      camera_change_pub.Publish(camera->GetViewProjection());
                       return true;
                     });
 
@@ -58,19 +52,24 @@ struct Application::Impl
                     event,
                     [&](const EvData& ev)
                     {
-                      const auto& [_, k] = *std::get_if<KeyPressData>(&ev);
-                      const float step = 0.25f;
-                      if (k == GLFW_KEY_W)
-                        cam_pos.z -= step;
-                      if (k == GLFW_KEY_S)
-                        cam_pos.z += step;
-                      if (k == GLFW_KEY_A)
-                        cam_pos.x -= step;
-                      if (k == GLFW_KEY_D)
-                        cam_pos.x += step;
-
-                      camera = Camera{ cam_pos, pitch, yaw, 0 };
-                      camera_change_pub.Publish(camera.GetViewProjection());
+                      // const auto& [_, k] = *std::get_if<KeyPressData>(&ev);
+                      // const float step = 0.25f;
+                      // if (k == GLFW_KEY_W)
+                      //   cam_pos.z -= step;
+                      // else if (k == GLFW_KEY_S)
+                      //   cam_pos.z += step;
+                      // else if (k == GLFW_KEY_A)
+                      //   cam_pos.x -= step;
+                      // else if (k == GLFW_KEY_D)
+                      //   cam_pos.x += step;
+                      // else if (k == GLFW_KEY_Q)
+                      //   cam_pos.y += step * 4;
+                      // else if (k == GLFW_KEY_E)
+                      //   cam_pos.y -= step * 4;
+                      //
+                      // camera = Camera{ cam_pos, pitch, yaw };
+                      // camera_change_pub.Publish(
+                      //   camera.GetViewProjection(window->GetWidth(), window->GetHeight()));
                       return true;
                     });
 
@@ -81,8 +80,7 @@ struct Application::Impl
                       const auto& [_, bt] = *std::get_if<MouseButtonPressData>(&ev);
                       if (bt == GLFW_MOUSE_BUTTON_1)
                       {
-                        is_mouse_pressed = true;
-                        mouse_press_pos = window->GetCursorPos();
+                        camera->StartMovement(window->GetCursorPos());
                       }
                       return true;
                     });
@@ -94,29 +92,9 @@ struct Application::Impl
                       const auto& [_, bt] = *std::get_if<MouseButtonReleaseData>(&ev);
                       if (bt == GLFW_MOUSE_BUTTON_1)
                       {
-                        is_mouse_pressed = false;
-
-                        const float diff_x = mouse_press_pos.x - window->GetCursorPos().x;
-                        const float diff_y = mouse_press_pos.y - window->GetCursorPos().y;
-
-                        const float diff_x_limited =
-                          std::clamp(diff_x,
-                                     (float)(window->GetWidth() / -2.0f),
-                                     (float)(window->GetWidth() / 2.0f)) /
-                          400.0f;
-                        const float diff_y_limited =
-                          std::clamp(diff_y,
-                                     (float)(window->GetHeight() / -2.0f),
-                                     (float)(window->GetHeight() / 2.0f)) /
-                          400.0f;
-
-                        const float diff_pitch = +diff_y_limited * 50;
-                        const float diff_yaw = -diff_x_limited * 50;
-
-                        pitch += diff_pitch;
-                        yaw += diff_yaw;
-
-                        printf("%1.3f, %1.3f\n", pitch, yaw);
+                        camera->StopMovement();
+                        camera->MeasureMovement(window->GetCursorPos());
+                        camera_change_pub.Publish(camera->GetViewProjection());
                       }
                       return true;
                     });
@@ -125,30 +103,12 @@ struct Application::Impl
                     event,
                     [&](const EvData& ev)
                     {
-                      if (!is_mouse_pressed)
+                      if (!camera->IsMoving())
                         return false;
+
                       const auto& [_, x, y] = *std::get_if<MouseMoveData>(&ev);
-
-                      const float diff_x = mouse_press_pos.x - x;
-                      const float diff_y = mouse_press_pos.y - y;
-
-                      const float diff_x_limited = std::clamp(diff_x,
-                                                              (float)(window->GetWidth() / -2.0f),
-                                                              (float)(window->GetWidth() / 2.0f)) /
-                                                   400.0f;
-                      const float diff_y_limited = std::clamp(diff_y,
-                                                              (float)(window->GetHeight() / -2.0f),
-                                                              (float)(window->GetHeight() / 2.0f)) /
-                                                   400.0f;
-
-                      const float diff_pitch = +diff_y_limited * 50;
-                      const float diff_yaw = -diff_x_limited * 50;
-
-                      printf("%1.3f, %1.3f\n", diff_pitch, diff_yaw);
-
-                      camera = Camera{ cam_pos, pitch + diff_pitch, yaw + diff_yaw, 0 };
-                      camera_change_pub.Publish(camera.GetViewProjection());
-
+                      camera->MeasureMovement({ x, y });
+                      camera_change_pub.Publish(camera->GetViewProjection());
                       return true;
                     });
   }
@@ -166,15 +126,15 @@ Application::Application(std::string&& title, u32 width, u32 height)
 
   m_pimpl->window = MakeScope<Window>(WindowProps{ title, width, height },
                                       [this](Event& e) { m_pimpl->OnEvent(e); });
+  m_pimpl->camera = MakeScope<Camera>(static_cast<float>(width) / static_cast<float>(height),
+                                      Vec3{ 5, 2, 5 },
+                                      180.0f,
+                                      0.0f);
 
   Renderer::Init();
   Renderer::SetViewport(0, 0, width, height);
 
   ShadersLibrary::Get().SubToCameraPub(m_pimpl->camera_change_pub);
-  ShadersLibrary::Get().SubToResizePub(m_pimpl->resize_change_pub);
-
-  m_pimpl->tex = MakeScope<Texture2D>("assets/dice.jpg");
-  m_pimpl->tex->Bind(0);
 
   //  {
   ////    Vec3 eye{ 0.0f, 4.0f, 4.0f };
@@ -188,18 +148,7 @@ Application::Application(std::string&& title, u32 width, u32 height)
   //    };
   //  }
 
-  m_pimpl->camera_change_pub.Publish(m_pimpl->camera.GetViewProjection());
-
-  m_pimpl->resize_change_pub.Publish(
-    Vec2{ (float)m_pimpl->window->GetWidth(), (float)m_pimpl->window->GetHeight() });
-
-  //  {
-  //    auto sh = std::static_pointer_cast<PosAndColorShader>(
-  //      ShadersLibrary::Get().GetShader(Shaders::POSITION_AND_COLOR));
-  //    sh->Activate();
-  //    sh->UploadWidth((float)m_pimpl->window->GetWidth());
-  //    sh->UploadHeight((float)m_pimpl->window->GetHeight());
-  //  }
+  m_pimpl->camera_change_pub.Publish(m_pimpl->camera->GetViewProjection());
 }
 
 Application::~Application() = default;
@@ -210,6 +159,7 @@ void Application::Run(const std::function<void(Window&)>& onLoop)
   {
     if (!m_pimpl->minimized)
     {
+
       onLoop(std::ref(*m_pimpl->window));
 
       m_pimpl->window->OnUpdate();
