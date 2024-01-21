aa1dc00681f575cc56483bd332422e25f1e20d25
Vinicius Almada <viniciusalmada10@gmail.com>
2024-01-21 10:00:01 -0300

    create function to build the BufferLayout elements based on type

--- src/ge_buffer_layout.cpp
+++ src/ge_buffer_layout.cpp
@@ -2,15 +2,49 @@
 
 using namespace GE;
 
+namespace
+{
+  u64 GetShaderDataTypeSize(ShaderDataType t)
+  {
+    switch (t)
+    {
+    case ShaderDataType::None:
+      return 0;
+    case ShaderDataType::Float:
+      return sizeof(float);
+    case ShaderDataType::Float2:
+      return sizeof(float) * 2;
+    case ShaderDataType::Float3:
+      return sizeof(float) * 3;
+    case ShaderDataType::Float4:
+      return sizeof(float) * 4;
+    case ShaderDataType::Mat3:
+      return sizeof(float) * 3 * 3;
+    case ShaderDataType::Mat4:
+      return sizeof(float) * 4 * 4;
+    case ShaderDataType::Int:
+      return sizeof(int);
+    case ShaderDataType::Int2:
+      return sizeof(int) * 2;
+    case ShaderDataType::Int3:
+      return sizeof(int) * 3;
+    case ShaderDataType::Int4:
+      return sizeof(int) * 4;
+    case ShaderDataType::Bool:
+      return 1;
+    }
+  }
+}
+
 struct BufferLayout::Impl
 {
   std::vector<BufferElem> elements;
   u32 stride = 0;
 };
 
-BufferLayout::BufferLayout(std::initializer_list<BufferElem> list) : m_pimpl(MakeScope<Impl>())
+BufferLayout::BufferLayout(std::list<BufferElem> list) : m_pimpl(MakeScope<Impl>())
 {
-  m_pimpl->elements = list;
+  m_pimpl->elements = { list.begin(), list.end() };
   u32 offset = 0;
   m_pimpl->stride = 0;
 
@@ -41,6 +75,20 @@ void BufferLayout::ForEachElement(const std::function<void(BufferElem)>& action)
 u32 BufferLayout::GetStride() const
 {
   return m_pimpl->stride;
+}
+
+std::list<BufferElem>
+GE::BufferLayout::BuildElementsList(std::initializer_list<ShaderDataType> types)
+{
+  std::list<BufferElem> elems{};
+  u64 offset = 0;
+  for (ShaderDataType type : types)
+  {
+    auto size = GetShaderDataTypeSize(type);
+    elems.emplace_back(type, size, offset, false);
+    offset += size;
+  }
+  return elems;
 }
 
 BufferLayout::~BufferLayout() = default;
