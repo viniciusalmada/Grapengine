0fdcae0d9ca63ba6c2720b9d738bad75e7b878a0
Vinicius Almada <viniciusalmada10@gmail.com>
2024-01-21 00:25:56 -0300

    remove tests code from Application

--- src/ge_application.cpp
+++ src/ge_application.cpp
@@ -12,118 +12,18 @@
 
 using namespace GE;
 
-class CameraChangePub : public IPublisher<Mat4>
-{
-};
-
 struct Application::Impl
 {
   Scope<Window> window;
   bool running = true;
   bool minimized = false;
-  Scope<Camera> camera;
-  Ref<Texture2D> color_tex;
-  CameraChangePub camera_change_pub{};
-  Vec2 mouse_press_pos{ 0, 0 };
-
-  static Application* instance;
 
   void Finish() { running = false; }
-
-  void OnEvent(Event& event)
-  {
-    Event::Dispatch(EvType::WINDOW_CLOSE,
-                    event,
-                    [this](auto&&)
-                    {
-                      Finish();
-                      return true;
-                    });
-
-    Event::Dispatch(EvType::WINDOW_RESIZE,
-                    event,
-                    [this](const EvData& ev)
-                    {
-                      const auto& [_, w, h] = *std::get_if<WindowResizeData>(&ev);
-                      Renderer::SetViewport(0, 0, w, h);
-                      camera->SetAspectRatio(static_cast<float>(w) / static_cast<float>(h));
-                      camera_change_pub.Publish(camera->GetViewProjection());
-                      return true;
-                    });
-
-    Event::Dispatch(EvType::KEY_PRESS,
-                    event,
-                    [&](const EvData& ev)
-                    {
-                      // const auto& [_, k] = *std::get_if<KeyPressData>(&ev);
-                      // const float step = 0.25f;
-                      // if (k == GLFW_KEY_W)
-                      //   cam_pos.z -= step;
-                      // else if (k == GLFW_KEY_S)
-                      //   cam_pos.z += step;
-                      // else if (k == GLFW_KEY_A)
-                      //   cam_pos.x -= step;
-                      // else if (k == GLFW_KEY_D)
-                      //   cam_pos.x += step;
-                      // else if (k == GLFW_KEY_Q)
-                      //   cam_pos.y += step * 4;
-                      // else if (k == GLFW_KEY_E)
-                      //   cam_pos.y -= step * 4;
-                      //
-                      // camera = Camera{ cam_pos, pitch, yaw };
-                      // camera_change_pub.Publish(
-                      //   camera.GetViewProjection(window->GetWidth(), window->GetHeight()));
-                      return true;
-                    });
-
-    Event::Dispatch(EvType::MOUSE_BUTTON_PRESSED,
-                    event,
-                    [&](const EvData& ev)
-                    {
-                      const auto& [_, bt] = *std::get_if<MouseButtonPressData>(&ev);
-                      if (bt == GLFW_MOUSE_BUTTON_1)
-                      {
-                        camera->StartMovement(window->GetCursorPos());
-                      }
-                      return true;
-                    });
-
-    Event::Dispatch(EvType::MOUSE_BUTTON_RELEASE,
-                    event,
-                    [&](const EvData& ev)
-                    {
-                      const auto& [_, bt] = *std::get_if<MouseButtonReleaseData>(&ev);
-                      if (bt == GLFW_MOUSE_BUTTON_1)
-                      {
-                        camera->StopMovement();
-                        camera->MeasureMovement(window->GetCursorPos());
-                        camera_change_pub.Publish(camera->GetViewProjection());
-                      }
-                      return true;
-                    });
-
-    Event::Dispatch(EvType::MOUSE_MOVE,
-                    event,
-                    [&](const EvData& ev)
-                    {
-                      if (!camera->IsMoving())
-                        return false;
-
-                      const auto& [_, x, y] = *std::get_if<MouseMoveData>(&ev);
-                      camera->MeasureMovement({ x, y });
-                      camera_change_pub.Publish(camera->GetViewProjection());
-                      return true;
-                    });
-  }
 };
-
-Application* Application::Impl::instance = nullptr;
 
 Application::Application(std::string&& title, u32 width, u32 height, std::string&& icon)
 {
   m_pimpl = MakeScope<Impl>();
-  if (m_pimpl->instance != nullptr)
-    throw std::exception("App already instantiated!");
 
   m_pimpl->instance = this;
 
@@ -136,13 +36,6 @@ Application::Application(std::string&& title, u32 width, u32 height, std::string
 
   Renderer::Init();
   Renderer::SetViewport(0, 0, width, height);
-
-  ShadersLibrary::Get().SubToCameraPub(m_pimpl->camera_change_pub);
-
-  m_pimpl->color_tex = MakeScope<Texture2D>();
-  m_pimpl->color_tex->Bind(1);
-
-  m_pimpl->camera_change_pub.Publish(m_pimpl->camera->GetViewProjection());
 }
 
 Application::~Application() = default;
@@ -153,8 +46,6 @@ void Application::Run(const std::function<void(Window&)>& onLoop) const
   {
     if (!m_pimpl->minimized)
     {
-      if (onLoop)
-        onLoop(std::ref(*m_pimpl->window));
 
       m_pimpl->window->OnUpdate();
     }
