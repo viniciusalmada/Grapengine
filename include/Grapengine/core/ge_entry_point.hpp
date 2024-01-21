abdb67a60ab80027ca5fce8e789a16a569e63de5
Vinicius Almada <viniciusalmada10@gmail.com>
2024-01-20 22:54:08 -0300

    create the EntryPoint inside lib code

---
+++ include/Grapengine/core/ge_entry_point.hpp
@@ -0,0 +1,17 @@
+#ifndef GE_ENTRY_POINT_HPP
+#define GE_ENTRY_POINT_HPP
+
+#include "core/ge_application.hpp"
+#include "core/ge_memory.hpp"
+
+extern GE::Scope<GE::Application> CreateApplication();
+
+int main(int argc, char* argv[])
+{
+  {
+    auto app = CreateApplication();
+    app->Run({});
+  }
+}
+
+#endif // GE_ENTRY_POINT_HPP
