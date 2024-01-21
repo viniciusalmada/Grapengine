f50b6ecd3ca741e78a1fb37cf0593283c918ea83
Vinicius Almada <viniciusalmada10@gmail.com>
2024-01-21 10:03:38 -0300

    create a WorldReference object to be used as basic draw

---
+++ include/Grapengine/drawables/ge_world_reference.hpp
@@ -0,0 +1,22 @@
+#ifndef GRAPENGINE_GE_WORLD_REFERENCE_HPP
+#define GRAPENGINE_GE_WORLD_REFERENCE_HPP
+
+#include "core/ge_macros.hpp"
+#include "core/ge_type_aliases.hpp"
+
+namespace GE
+{
+  class WorldReference
+  {
+  public:
+    GE3D WorldReference();
+    GE3D ~WorldReference();
+
+    GE3D void DrawBatch();
+
+  private:
+    POINTER_TO_IMPLEMENTATION_IDIOM
+  };
+}
+
+#endif // GRAPENGINE_GE_WORLD_REFERENCE_HPP
