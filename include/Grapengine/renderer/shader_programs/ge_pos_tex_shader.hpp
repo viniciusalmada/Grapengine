415b96fa5e24f2a1a340b4bfe049a6be3600e556
Vinicius Almada <viniciusalmada10@gmail.com>
2024-01-21 09:58:52 -0300

    change ge_pos_tex_shader.cpp to be single texture and pass uniform by parameters

--- include/Grapengine/renderer/shader_programs/ge_pos_tex_shader.hpp
+++ include/Grapengine/renderer/shader_programs/ge_pos_tex_shader.hpp
@@ -7,20 +7,21 @@
 #include <utils/ge_ipubsub.hpp>
 namespace GE
 {
-class PosAndTex2DShader : public IShaderProgram, public ISubscriber<Mat4>
-{
-public:
-  PosAndTex2DShader();
-  ~PosAndTex2DShader();
+  class PosAndTex2DShader : public IShaderProgram
+  {
+  public:
+    PosAndTex2DShader();
+    ~PosAndTex2DShader();
 
-  void Activate() override;
-  void Deactivate() override;
+    void Activate() override;
+    void Deactivate() override;
 
-  void OnUpdate(Mat4 data) override;
-//  void OnUpdate(i32 data) override;
+    void UpdateModelMatrix(Mat4 model);
+    void UpdateViewProjectionMatrix(Mat4 viewProj);
+    void UpdateTexture(int id);
 
-private:
-  POINTER_TO_IMPLEMENTATION_IDIOM
-};
+  private:
+    POINTER_TO_IMPLEMENTATION_IDIOM
+  };
 }
 #endif // GRAPENGINE_POS_TEX_SHADER_HPP
--- src/ge_pos_tex_shader.cpp
+++ src/ge_pos_tex_shader.cpp
@@ -11,20 +11,18 @@ namespace
     
     layout (location = 0) in vec3 in_position;
     layout (location = 1) in vec2 in_texture_coord;
-    layout (location = 2) in float in_use_color;
-    layout (location = 3) in vec4 in_color;
+    layout (location = 2) in vec4 in_color;
 
     out vec2 out_texture_coords;
-    out float out_use_color;
     out vec4 out_color;
 
+    uniform mat4 u_M;
     uniform mat4 u_VP;
 
     void main()
     {
-      gl_Position = (u_VP) * vec4(in_position, 1.0);
+      gl_Position = (u_VP) * u_M * vec4(in_position, 1.0);
       out_texture_coords = in_texture_coord;
-      out_use_color = in_use_color;
       out_color = in_color;
     }
 )glsl";
@@ -33,20 +31,15 @@ namespace
     #version 330 core
 
     in vec2 out_texture_coords;
-    in float out_use_color;
     in vec4 out_color;
 
     out vec4 fragColor;
 
-    uniform sampler2D img_texture;
-    uniform sampler2D color_texture;
+    uniform sampler2D u_texture;
 
     void main()
     {
-      if (out_use_color > 0.5)
-        fragColor = texture2D(color_texture, vec2(0,0)) * out_color;
-      else
-        fragColor = texture2D(img_texture, out_texture_coords);
+      fragColor = texture2D(u_texture, out_texture_coords) * out_color;
     }
 )glsl";
 }
@@ -59,11 +52,6 @@ struct PosAndTex2DShader::Impl
 PosAndTex2DShader::PosAndTex2DShader() : m_pimpl(MakeScope<Impl>())
 {
   m_pimpl->shader = MakeScope<Shader>(V_SHADER, F_SHADER);
-  // TODO: TEMP
-  {
-    Activate();
-    m_pimpl->shader->UploadInt("color_texture", 1);
-  }
 }
 
 PosAndTex2DShader::~PosAndTex2DShader() = default;
@@ -71,8 +59,6 @@ PosAndTex2DShader::~PosAndTex2DShader() = default;
 void PosAndTex2DShader::Activate()
 {
   m_pimpl->shader->Bind();
-  m_pimpl->shader->UploadInt("img_texture", 0);
-
 }
 
 void PosAndTex2DShader::Deactivate()
@@ -80,8 +66,20 @@ void PosAndTex2DShader::Deactivate()
   m_pimpl->shader->Unbind();
 }
 
-void PosAndTex2DShader::OnUpdate(Mat4 data)
+void PosAndTex2DShader::UpdateModelMatrix(Mat4 data)
 {
   Activate();
-  m_pimpl->shader->UploadMat4F("u_VP", data);
+  m_pimpl->shader->UploadMat4F("u_M", data);
+}
+
+void PosAndTex2DShader::UpdateViewProjectionMatrix(Mat4 viewProj)
+{
+  Activate();
+  m_pimpl->shader->UploadMat4F("u_VP", viewProj);
+}
+
+void PosAndTex2DShader::UpdateTexture(int id)
+{
+  Activate();
+  m_pimpl->shader->UploadInt("u_texture", id);
 }
