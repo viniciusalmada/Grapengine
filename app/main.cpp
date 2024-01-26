03e4787ee6ddd8d7275644408f944d63e885a0b0
Vinicius Almada <viniciusalmada10@gmail.com>
2024-01-25 22:45:20 -0300

    app: configure WorldReference

--- app/main.cpp
+++ app/main.cpp
@@ -1,10 +1,34 @@
 #include "Grapengine/grapengine.hpp"
 #include "core/ge_entry_point.hpp"
 
+#include <iostream>
+
+class SimpleLayer : public GE::Layer
+{
+public:
+  SimpleLayer() : GE::Layer("Simple") {}
+
+  void OnAttach() override { m_world = GE::MakeScope<GE::WorldReference>(); }
+
+  void OnUpdate(GE::TimeStep ts) override
+  {
+    GE::Renderer::SetClearColor(GE::Color{ 0x222222FF }.ToVec4());
+    GE::Renderer::Clear();
+    m_world->DrawBatch();
+  }
+
+private:
+  GE::Scope<GE::WorldReference> m_world = nullptr;
+};
+
 class Client : public GE::Application
 {
 public:
-  Client() : Application("Grapengine", 1280, 720, "assets/ic_grapengine.png") {}
+  Client() : Application("Grapengine", 1280, 720, "assets/ic_grapengine.png")
+  {
+    GE::Ref<SimpleLayer> simple = GE::MakeRef<SimpleLayer>();
+    this->AddLayer(simple);
+  }
 };
 
 GE::Scope<GE::Application> CreateApplication()
