2132044f1778b1f9ae61dfc4e51134c01617cdfc
Vinicius Almada <viniciusalmada10@gmail.com>
2024-02-17 21:52:46 -0300

    dev: enable ge_circle_shape in cmakelists

--- src/ge_circle_shape.cpp
+++ src/ge_circle_shape.cpp
@@ -15,7 +15,7 @@ struct CircleShape::Impl
   float position_x = 0;
   float position_y = 0;
   float radius = 0;
-  Color color{};
+  Color color{ Colors::WHITE };
   Ref<DrawPrimitive> draw_primitive;
   Shaders shader;
 };
@@ -30,16 +30,12 @@ CircleShape::CircleShape(float x, float y, float radius, Color color, Shaders sh
   m_pimpl->shader = shader;
 
   auto layout = MakeRef<BufferLayout>(std::initializer_list<BufferElem>{
-    BufferElem{ "in_position", ShaderDataType::Float3, sizeof(float) * 3, 0, false },
-    BufferElem{ "in_color",
-                ShaderDataType::Float4,
-                sizeof(float) * 4,
-                sizeof(float) * 3,
-                false } });
+    BufferElem{ ShaderDataType::Float3, sizeof(float) * 3, 0, false },
+    BufferElem{ ShaderDataType::Float4, sizeof(float) * 4, sizeof(float) * 3, false } });
 
   auto positions = MakeRef<VerticesData>(layout);
 
-  positions->PushData(Vec3{ x, y, -1000}, color.ToVec4());
+  positions->PushData(Vec3{ x, y, -1000 }, color.ToVec4());
 
   auto indices = MakeRef<std::vector<u32>>();
   indices->reserve(CIRCLE_POINTS * 3);
@@ -48,8 +44,8 @@ CircleShape::CircleShape(float x, float y, float radius, Color color, Shaders sh
   for (u32 i = 0; i < CIRCLE_POINTS; ++i)
   {
     const float angle = (float)i / total * 2 * std::numbers::pi_v<float>;
-    const float circle_x = cos(angle) * radius + x;
-    const float circle_y = sin(angle) * radius + y;
+    const float circle_x = std::cos(angle) * radius + x;
+    const float circle_y = std::sin(angle) * radius + y;
     auto pos = Vec3(circle_x, circle_y, 0.0f);
     positions->PushData(pos, color.ToVec4());
 
