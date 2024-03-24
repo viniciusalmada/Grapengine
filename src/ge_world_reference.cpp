c9e4cd92e2064103d35af6bf729f9b4507bcb3bb
Vinicius Almada <viniciusalmada10@gmail.com>
2024-03-24 16:26:49 -0300

    dev: remove 2D rectangle shape

--- src/ge_world_reference.cpp
+++ src/ge_world_reference.cpp
@@ -2,7 +2,6 @@
 
 #include <drawables/ge_cube.hpp>
 #include <drawables/ge_cylinder.hpp>
-#include <drawables/ge_rect_shape.hpp>
 #include <renderer/ge_shaders_library.hpp>
 #include <renderer/ge_texture_2d.hpp>
 
@@ -22,8 +21,6 @@ struct WorldReference::Impl
   Ref<Cylinder> x_axis;
   Ref<Cylinder> y_axis;
   Ref<Cylinder> z_axis;
-  Ref<RectShape> xy_plane;
-  Ref<RectShape> yz_plane;
 };
 
 GE::WorldReference::WorldReference() : m_pimpl(MakeScope<Impl>())
@@ -55,22 +52,6 @@ GE::WorldReference::WorldReference() : m_pimpl(MakeScope<Impl>())
                                       SIDE_SIZE * 10,
                                       Colors::BLUE,
                                       m_pimpl->blank_texture);
-
-  m_pimpl->xy_plane = MakeRef<RectShape>(Vec3{ 0, 0, 0 },
-                                         Vec3{ SIDE_SIZE, 0, 0 },
-                                         Vec3{ SIDE_SIZE, SIDE_SIZE, 0 },
-                                         Vec3{ 0, SIDE_SIZE, 0 },
-                                         Color{ 0xFFFF3388 },
-                                         Shaders::POSITION_AND_TEXTURE2D,
-                                         m_pimpl->blank_texture);
-
-  m_pimpl->yz_plane = MakeRef<RectShape>(Vec3{ 0, 0, 0 },
-                                         Vec3{ 0, 0, SIDE_SIZE },
-                                         Vec3{ 0, SIDE_SIZE, SIDE_SIZE },
-                                         Vec3{ 0, SIDE_SIZE, 0 },
-                                         Color{ 0x33FF3388 },
-                                         Shaders::POSITION_AND_TEXTURE2D,
-                                         m_pimpl->blank_texture);
 }
 
 void WorldReference::DrawBatch() const
@@ -79,8 +60,6 @@ void WorldReference::DrawBatch() const
   m_pimpl->x_axis->Draw();
   m_pimpl->y_axis->Draw();
   m_pimpl->z_axis->Draw();
-  //  m_pimpl->xy_plane->Draw();
-  //  m_pimpl->yz_plane->Draw();
 }
 
 WorldReference::~WorldReference() = default;
