9d02e5b272090cabb1f13fe139d86eecc7675f97
Vinicius Almada <viniciusalmada10@gmail.com>
2024-03-29 17:24:46 -0300

    ref: move functions implementations from Application to PIMPL struct

--- src/ge_application.cpp
+++ src/ge_application.cpp
@@ -24,42 +24,58 @@ struct Application::Impl
   bool minimized = false;
   u64 last_frame_time{ 0 };
   std::vector<Ref<Layer>> layers;
-  // Scope<Camera> camera;
+
+  void Init(std::string_view title,
+            u32 width,
+            u32 height,
+            std::string_view icon,
+            const EventCallbackFn& cb)
+  {
+    window = MakeScope<Window>(WindowProps{ title, width, height, icon }, cb);
+    input = MakeScope<Input>(window);
+  }
+
+  void OnEvent(Event& event)
+  {
+    for (const auto& layer : layers)
+      layer->OnEvent(event);
+
+    Event::Dispatch(EvType::WINDOW_CLOSE,
+                    event,
+                    [this](auto&&)
+                    {
+                      Finish();
+                      return true;
+                    });
+
+    Event::Dispatch(EvType::WINDOW_RESIZE,
+                    event,
+                    [](const EvData& ev)
+                    {
+                      const auto& [_, w, h] = *std::get_if<WindowResizeData>(&ev);
+                      Renderer::SetViewport(0, 0, w, h);
+                      return true;
+                    });
+  }
+
+  void Run()
+  {
+    while (running)
+    {
+      u64 time_ms = Platform::GetCurrentTimeMS();
+      TimeStep step{ time_ms - last_frame_time };
+      last_frame_time = time_ms;
+
+      if (!minimized)
+      {
+        std::ranges::for_each(layers, [&](auto&& l) { l->OnUpdate(step); });
+      }
+
+      window->OnUpdate();
+    }
+  }
 
   void Finish() { running = false; }
-
-  // void OnMousePress(KeyCode bt) const
-  // {
-  //   if (!camera->IsAiming() && bt == KeyCode::MOUSE_BT_LEFT)
-  //     camera->StartAiming(input->GetMouseXY());
-  //
-  //   if (!camera->IsAiming() && bt == KeyCode::MOUSE_BT_MIDDLE)
-  //     camera->StartMoving(input->GetMouseXY());
-  // }
-  //
-  // void OnMouseMove(f32 x, f32 y) const
-  // {
-  //   if (camera->IsAiming())
-  //     camera->ChangeAimPoint({ x, y });
-  //   if (camera->IsMoving())
-  //     camera->ChangeLocation({ x, y });
-  // }
-  //
-  // void OnMouseRelease(KeyCode bt) const
-  // {
-  //   if (camera->IsAiming() && bt == KeyCode::MOUSE_BT_LEFT)
-  //   {
-  //     camera->StopAiming();
-  //     camera->ChangeAimPoint(input->GetMouseXY());
-  //   }
-  //   if (camera->IsMoving() && bt == KeyCode::MOUSE_BT_MIDDLE)
-  //   {
-  //     camera->StopMoving();
-  //     camera->ChangeLocation(input->GetMouseXY());
-  //   }
-  // }
-  //
-  // void OnMouseScroll(f32 diffY) const { camera->SetZoom(-diffY); }
 };
 
 Application::Application(std::string_view title, u32 width, u32 height, std::string_view icon)
@@ -67,16 +83,7 @@ Application::Application(std::string_view title, u32 width, u32 height, std::str
   GE_INFO("Application creation")
 
   m_pimpl = MakeScope<Impl>();
-
-  m_pimpl->window =
-    MakeScope<Window>(WindowProps{ title, width, height, icon }, [this](Event& e) { OnEvent(e); });
-  m_pimpl->input = MakeScope<Input>(m_pimpl->window);
-
-  // const f32 aspectRatio = f32(width) / (f32)height;
-  // m_pimpl->camera = MakeScope<Camera>(aspectRatio, Vec3{ 0, 5, 5 }, 223.0f, -40);
-  //  auto shader = std::static_pointer_cast<PosAndTex2DShader>(
-  //    ShadersLibrary::Get().GetShader(Shaders::POSITION_AND_TEXTURE2D));
-  // shader->UpdateViewProjectionMatrix(m_pimpl->camera->GetViewProjection());
+  m_pimpl->Init(title, width, height, icon, [this](auto&& e) { OnEvent(e); });
 
   Renderer::Init();
   Renderer::SetViewport(0, 0, width, height);
@@ -86,90 +93,12 @@ Application::~Application() = default;
 
 void Application::Run() const
 {
-  while (m_pimpl->running)
-  {
-    u64 time_ms = Platform::GetCurrentTimeMS();
-    TimeStep step{ time_ms - m_pimpl->last_frame_time };
-    m_pimpl->last_frame_time = time_ms;
-
-    if (!m_pimpl->minimized)
-    {
-      // if (m_pimpl->camera->IsAiming() || m_pimpl->camera->IsMoving())
-      // {
-      //   auto shader = std::static_pointer_cast<PosAndTex2DShader>(
-      //     ShadersLibrary::Get().GetShader(Shaders::POSITION_AND_TEXTURE2D));
-      //   shader->UpdateViewProjectionMatrix(m_pimpl->camera->GetViewProjection());
-      // }
-
-      std::ranges::for_each(m_pimpl->layers, [&](auto&& l) { l->OnUpdate(step); });
-    }
-
-    m_pimpl->window->OnUpdate();
-  }
+  m_pimpl->Run();
 }
-
-// f32 x = -1;
-// f32 y = -1;
 
 void GE::Application::OnEvent(Event& event)
 {
-  for (const auto& layer : m_pimpl->layers)
-    layer->OnEvent(event);
-
-  Event::Dispatch(EvType::WINDOW_CLOSE,
-                  event,
-                  [this](auto&&)
-                  {
-                    m_pimpl->Finish();
-                    return true;
-                  });
-
-  Event::Dispatch(EvType::WINDOW_RESIZE,
-                  event,
-                  [](const EvData& ev)
-                  {
-                    const auto& [_, w, h] = *std::get_if<WindowResizeData>(&ev);
-                    Renderer::SetViewport(0, 0, w, h);
-                    return true;
-                  });
-  // Event::Dispatch(EvType::MOUSE_BUTTON_PRESSED,
-  //                 event,
-  //                 [this](const EvData& ev)
-  //                 {
-  //                   const auto& [_, bt] = *std::get_if<MouseButtonPressData>(&ev);
-  //                   m_pimpl->OnMousePress(bt);
-  //                   return true;
-  //                 });
-  //
-  // Event::Dispatch(EvType::MOUSE_BUTTON_RELEASE,
-  //                 event,
-  //                 [this](const EvData& ev)
-  //                 {
-  //                   const auto& [_, bt] = *std::get_if<MouseButtonReleaseData>(&ev);
-  //                   m_pimpl->OnMouseRelease(bt);
-  //                   return true;
-  //                 });
-  //
-  // Event::Dispatch(EvType::MOUSE_MOVE,
-  //                 event,
-  //                 [this](const EvData& ev)
-  //                 {
-  //                   const auto& [_, x, y] = *std::get_if<MouseMoveData>(&ev);
-  //                   m_pimpl->OnMouseMove(x, y);
-  //                   return true;
-  //                 });
-  //
-  // Event::Dispatch(EvType::MOUSE_SCROLL,
-  //                 event,
-  //                 [this](const EvData& ev)
-  //                 {
-  //                   const auto& [_, x, y] = *std::get_if<MouseScrollData>(&ev);
-  //                   m_pimpl->OnMouseScroll(y);
-  //                   auto shader = std::static_pointer_cast<PosAndTex2DShader>(
-  //                     ShadersLibrary::Get().GetShader(Shaders::POSITION_AND_TEXTURE2D));
-  //                   shader->UpdateViewProjectionMatrix(m_pimpl->camera->GetViewProjection());
-  //                   return true;
-  //                 });
+  m_pimpl->OnEvent(event);
 }
 
 void GE::Application::AddLayer(const Ref<Layer>& layer) const
