d19a90f08b83611d10b96a85b2c9a55364d3e8a8
Vinicius Almada <viniciusalmada10@gmail.com>
2024-05-25 22:45:22 -0300

    ref: extract functions to split main function

--- Grapengine/core/ge_entry_point.hpp
+++ Grapengine/core/ge_entry_point.hpp
@@ -4,37 +4,46 @@
 #include "controllers/ge_app_controller.hpp"
 #include "core/ge_application.hpp"
 #include "core/ge_memory.hpp"
-#include "ge_assert.hpp"
 #include "profiling/ge_profiler.hpp"
 
 extern GE::Ref<GE::Application> CreateApplication();
 
-// NOLINT(*-definitions-in-headers)
-int main(int argc, char* argv[])
+namespace
 {
-  GE::Ref<GE::Application> app = nullptr;
+  GE::Ref<GE::Application> Start(int argc, char* argv[]) noexcept
   {
     GE_PROFILE_SECTION("Main init");
     GE::Logger::Init();
     auto args = std::span(argv, u64(argc));
-    GE_DEBUG("----------Startup at <{}>----------", args.front())
-    app = CreateApplication();
+    GE_DEBUG("Main: Starting up at \'{}\'", args.front())
+    auto app = CreateApplication();
     GE::Ctrl::App::Init(app);
+    return app;
   }
 
+  void Run(GE::Ref<GE::Application>& app) noexcept
   {
     GE_PROFILE_SECTION("Main running");
-    GE_DEBUG("----------Running----------")
+    GE_DEBUG("Main: Running")
     app->Run();
   }
 
-  GE_DEBUG("----------Shutdown----------")
+  void Shutdown(GE::Ref<GE::Application>& app) noexcept
   {
-    GE_PROFILE_SECTION("Main Shutdown");
+    GE_PROFILE_SECTION("Main: Shutdown");
+    GE_DEBUG("Shutdown")
     GE::Ctrl::App::Shutdown();
     app.reset();
     GE::Logger::Shutdown();
   }
+}
+
+// NOLINT(*-definitions-in-headers)
+int main(int argc, char* argv[])
+{
+  GE::Ref<GE::Application> app = Start(argc, argv);
+  Run(app);
+  Shutdown(app);
 }
 
 #endif // GE_ENTRY_POINT_HPP
