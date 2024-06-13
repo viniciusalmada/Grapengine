fb7cd7485ea31fd00f6f9df36b0bf66abdd70825
Vinicius Almada <viniciusalmada10@gmail.com>
2024-06-12 23:35:01 -0300

    dev: update camera controller script

--- Wineglass/nativescripts/CamController.cpp
+++ Wineglass/nativescripts/CamController.cpp
@@ -1,3 +1,69 @@
 #include "CamController.hpp"
 
+namespace
+{
+  const auto& MoveKeys()
+  {
+    static const std::set<KeyCode> MOVE_KEYS{ KeyCode::A, KeyCode::D, KeyCode::W,
+                                              KeyCode::S, KeyCode::E, KeyCode::Q };
+    return MOVE_KEYS;
+  }
+
+}
+
+CamController::CamController(Entity e, Scene& s) : ScriptableEntity(e, s) {}
+
+void CamController::OnCreate() {}
+
+void CamController::OnDestroy() {}
+
+void CamController::OnUpdate(TimeStep ts)
+{
+  if (std::ranges::none_of(MoveKeys(), [](auto&& k) { return Input::IsKeyPressed(k); }))
+    return;
+
+  constexpr f32 speed = 0.01f;
+  const f32 incr = speed * ts.f();
+
+  auto& camera_comp = m_scene.GetComponent<CameraComponent>(m_entity);
+  if (!camera_comp.active)
+    return;
+
+  auto cam_pos = camera_comp.camera.GetPosition();
+  auto tar_pos = camera_comp.camera.GetTarget();
+
+  if (Input::IsKeyPressed(KeyCode::A))
+  {
+    cam_pos.x -= incr;
+    tar_pos.x -= incr;
+  }
+  else if (Input::IsKeyPressed(KeyCode::D))
+  {
+    cam_pos.x += incr;
+    tar_pos.x += incr;
+  }
+  else if (Input::IsKeyPressed(KeyCode::W))
+  {
+    cam_pos.y += incr;
+    tar_pos.y += incr;
+  }
+  else if (Input::IsKeyPressed(KeyCode::S))
+  {
+    cam_pos.y -= incr;
+    tar_pos.y -= incr;
+  }
+  else if (Input::IsKeyPressed(KeyCode::E))
+  {
+    cam_pos.z -= incr;
+    tar_pos.z -= incr;
+  }
+  else if (Input::IsKeyPressed(KeyCode::Q))
+  {
+    cam_pos.z += incr;
+    tar_pos.z += incr;
+  }
+
+  camera_comp.camera.SetView(cam_pos, tar_pos);
+}
+
 CamController::~CamController() = default;
--- Wineglass/nativescripts/CamController.hpp
+++ Wineglass/nativescripts/CamController.hpp
@@ -8,47 +8,12 @@ using namespace GE;
 class CamController final : public ScriptableEntity
 {
 public:
-  CamController(Entity e, Scene& s) : ScriptableEntity(e, s), xyz_cam({ 0, 0, 10 }) {}
+  CamController(Entity e, Scene& s);
   ~CamController() override;
 
-  void OnCreate() override
-  {
-  }
-  void OnDestroy() override {}
-  void OnUpdate(TimeStep ts) override
-  {
-    constexpr f32 speed = 0.01f;
-    const f32 incr = speed * ts.f();
-
-    auto& camera_comp = m_scene.GetComponent<CameraComponent>(m_entity);
-    if (!camera_comp.active)
-      return;
-
-    if (Input::IsKeyPressed(KeyCode::A))
-      xyz_cam.x += incr;
-    else if (Input::IsKeyPressed(KeyCode::D))
-      xyz_cam.x -= incr;
-    else if (Input::IsKeyPressed(KeyCode::W))
-      xyz_cam.y -= incr;
-    else if (Input::IsKeyPressed(KeyCode::S))
-      xyz_cam.y += incr;
-    else if (Input::IsKeyPressed(KeyCode::E))
-      xyz_cam.z -= incr;
-    else if (Input::IsKeyPressed(KeyCode::Q))
-      xyz_cam.z += incr;
-
-    auto camera_pos = camera_comp.camera.GetPosition();
-    auto camera_tar = camera_comp.camera.GetTarget();
-    camera_pos.x = xyz_cam.x;
-    camera_pos.y = xyz_cam.y;
-    camera_pos.z = xyz_cam.z;
-    camera_tar.x = xyz_cam.x;
-    camera_tar.y = xyz_cam.y;
-    camera_comp.camera.SetView(camera_pos, camera_tar);
-  }
-
-private:
-  Vec3 xyz_cam;
+  void OnCreate() override;
+  void OnDestroy() override;
+  void OnUpdate(TimeStep ts) override;
 };
 
 #endif // GRAPENGINE_CAMCONTROLLER_HPP
